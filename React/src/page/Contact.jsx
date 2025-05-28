588af1d61058520c2e563129785c2c071aa60ff8
CodeCommandBD <shantokumar22@gmail.com>
2025-05-29 01:37:15 +0600

    update

--- React/src/page/Contact.jsx
+++ React/src/page/Contact.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import {useNavigate} from 'react-router-dom'
 const Contact = () => {
+
+  let navigate = useNavigate()
+
+  const handle = () => {
+      navigate('/')
+  }
+
   return (
     <div>
         <h1>contact</h1>
+        <button onClick={handle} className='p-3 bg-fuchsia-400 rounded-lg text-white cursor-pointer'>go back Home</button>
     </div>
   )
 }
