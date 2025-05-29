6d35f29a86168b4564aa6aa3d9709ed09f6bfee6
CodeCommandBD <shantokumar22@gmail.com>
2025-05-29 11:22:26 +0600

    update

--- React/src/page/Product.jsx
+++ React/src/page/Product.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Product = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://dummyjson.com/products')
+      const json = await response.json()
+      setProducts(json.products)
+
+
+    })()
+  }, [])
+
   return (
     <div>
-        <h1>product</h1>
+      <h1 className='text-3xl font-bold'>All The Products</h1>
+      <div>
+        {
+          products && products.length > 0 && products.map((product,i)=>{
+            return <div key={product.id}>
+                <h3>Title: {product.title}</h3>
+                <h3>Category: {product.category}</h3>
+                <h3>Price: {product.price}</h3>
+                <p>Des: {product.description.substring(0,100)}...</p>
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
--- React/src/page/UserProfile.jsx
+++ React/src/page/UserProfile.jsx
@@ -7,12 +7,17 @@ const UserProfile = () => {
 
   return (
     <div className='w-[40%] h-fit p-8 m-auto my-8 shadow-lg'>
+        { state ? 
         <div className=''>
             <h1 className='text-4xl font-bold mb-5'>User Profile</h1>
             <h4 className='text-xl'>Name: {state.name}</h4>
             <h4 className='text-xl'>Email: {state.email}</h4>
             <h4 className='text-xl'>Address: {state.address}</h4>
         </div>
+        :
+        <p>No profile exits</p>
+    
+        }
     </div>
   )
 }
