c1fd4ce9c9a9baede41bedc87a807ed849c1aa26
CodeCommandBD <shantokumar22@gmail.com>
2025-05-29 11:29:25 +0600

    update

--- React/src/page/Product.jsx
+++ React/src/page/Product.jsx
@@ -5,13 +5,17 @@ const Product = () => {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
+
     (async () => {
-      const response = await fetch('https://dummyjson.com/products')
-      const json = await response.json()
-      setProducts(json.products)
-
-
+      try {
+        const response = await fetch('https://dummyjson.com/products')
+        const json = await response.json()
+        setProducts(json.products)
+      } catch (err) {
+        console.error(err)
+      }
     })()
+
   }, [])
 
   return (
@@ -19,12 +23,13 @@ const Product = () => {
       <h1 className='text-3xl font-bold'>All The Products</h1>
       <div>
         {
-          products && products.length > 0 && products.map((product,i)=>{
-            return <div key={product.id}>
-                <h3>Title: {product.title}</h3>
-                <h3>Category: {product.category}</h3>
-                <h3>Price: {product.price}</h3>
-                <p>Des: {product.description.substring(0,100)}...</p>
+          products && products.length > 0 && products.map((product, i) => {
+            const { id, title, category, price, description } = product
+            return <div key={id}>
+              <h3>Title: {title}</h3>
+              <h3>Category: {category}</h3>
+              <h3>Price: {price}</h3>
+              <p>Des: {description && description.substring(0, 100)}...</p>
             </div>
           })
         }
